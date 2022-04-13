46e2f3f90133a555c9f8022b3b0e7575d8d37647
Michael Chilaka <obichilaka@gmail.com>
2022-04-13 15:36:23 +0100

    imported pusher variable, callback func on the pusher variable

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 import Cors from "cors"
 import Messages from "./dbMessages.js"
+import Pusher from "pusher";
 
 // APP CONFIG
 const app = express();
 const port = process.env.PORT || 9000;
 const connection_url = "mongodb+srv://mikky:siccoo2468@cluster0.ihspf.mongodb.net/whatsappDBase?retryWrites=true&w=majority";
+
+const pusher = new Pusher({
+    appId: "1384968",
+    key: "5af807256b897b25f136",
+    secret: "65100631119cd1f43cf7",
+    cluster: "eu",
+    useTLS: true
+});
 
 // MIDDLEWARE
 app.use(express.json());
@@ -19,6 +28,26 @@ mongoose.connect(connection_url, err => {
 })
 
 // API ROUTES
+const db = mongoose.connection
+db.once("once", () => {
+    console.log("DB Connected");
+    const msgCollection = db.collection("whatsappmessages")
+    const changeStream = msgCollection.watch()
+
+    changeStream.on("change", change => {
+        console.log(change);
+        if (change.operationType === "insert") {
+            const messageDetails = change.fullDocument
+            pusher.trigger("messages", "inserted", {
+                name: messageDetails.name,
+                message: messageDetails.message,
+                time: messageDetails.time,
+                received: messageDetails.received
+            })
+        }
+    })
+})
+
 app.get("/", (req, res) => res.status(200).send("WhatsApp MVP Clone - Lakajs"));
 
 app.get("/messages/sync", (req, res) => {
